c1b7aa627c048467510447dafa536c23b28ed738
Prathamesh Sawant <104883046+Prathamesh78@users.noreply.github.com>
2024-04-29 16:25:30 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,11 +1,11 @@
 from flask import Flask, send_from_directory
 
-app = Flask(__name__)
+app = Flask(__name__, template.folder='./templates')
 
 @app.route('/')
 def serve_html():
     return send_from_directory('static', 'sample.html')
 
 if __name__ == '__main__':
-    port = os.environ.get('PORT', 5000)
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
