9646ed6f5bb2f65b5a42f8fbb579b2b401afa244
Prathamesh Sawant <104883046+Prathamesh78@users.noreply.github.com>
2024-04-29 12:49:21 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -7,4 +7,5 @@ def serve_html():
     return send_from_directory('static', 'sample.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
