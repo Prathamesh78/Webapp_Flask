ba8fc8b8645abe11749e154f97261955f54b0929
Prathamesh Sawant <104883046+Prathamesh78@users.noreply.github.com>
2024-04-29 12:23:34 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def serve_html():
-    return send_from_directory('', 'sample.html')
+    return send_from_directory('static', 'sample.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
